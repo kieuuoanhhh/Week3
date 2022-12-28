f17171890cdc935aacf930f2829f328ab3ac62f6
kieuoanh <kieuoanh1001nb@gmail.com>
2022-12-28 17:05:31 +0700

    add

--- JavaScript/Data_Type.js
+++ JavaScript/Data_Type.js
@@ -154,8 +154,163 @@ var result= course.coin > 0 ? `${course.coin} Coins`:'Free'
 console.log(result)
 /* Vong lap */
 // For loop
-for(var i = 1; i <=100;i++){
+for(var i = 1; i <=10;i++){
     // code 
     console.log(i);
 
 }
+console.log(1/0)
+console.log(0/0)
+let name = 'Lam'
+console.log(`My name is ${name}`)
+// Vòng lặp for loop
+var myArray =[
+    'Jsr',
+    'php',
+    'lop']
+    console.log(myArray[0]);
+    var arrayLength = myArray.length;
+    for(var i=  0;i<arrayLength;i++)
+    {
+        console.log(myArray[i])
+    }
+    // For/in loop
+    var my = {
+        fiName : 'Kieu ',
+        age:18,
+        address:'HN'
+    };
+    for(var key in my){
+
+    }
+    var languages = 'javascr'
+    for(var key in languages){
+        console.log(languages[key])
+    }
+    // For/of loop
+    for(var value of myArray){
+        console.log(value)
+    }
+    console.log(Object.keys(my)) 
+    for(var value of Object.keys(my)){
+        console.log(value);
+    }
+    // while loop
+    var i = 0;
+    while(i<10){
+        i++;
+        console.log(i);
+    }
+    var array =[
+        'javaa',
+        'lop',
+        'aaa'
+    ]
+    var i = 0;
+    while(i<array.length){
+        console.log(array[i])
+        i++;
+    }
+    // do/while loop
+    var i = 0
+    do{
+        // code
+        i++;
+        console.log(i);
+    } while (i<10)
+    // Break  & Continue in Loop
+    for(var i = 0;i < 10;i++)
+    {
+        if(i%2 !== 0){
+            continue;
+        }
+        console.log(i)
+    }
+    var nest =[
+        [1, 2],
+        [3, 4],
+        [5, 6]
+    ];
+    for(var i = 0;i<nest.length;i++)
+    {
+        for(var j = 0;j<nest[i].length;j++){
+            console.log(nest[i][j])
+        }
+
+    }
+   //1. xac dinh diem stop
+   //2. logic handle => 
+   function countDown(num){
+    if(num > 0){
+        console.log(num);
+        return countDown(num - 1);
+
+    }
+    return num;
+   }
+   countDown(3)
+   /* mang */
+   //Tao mang
+
+   var languages = [
+    'Java',
+    'php',
+    'fp',
+    null,undefined
+   ];
+   console.log(languages[0])
+   // function
+   function writeLog(message,message2){
+        console.log(message,message2)
+   }
+   writeLog('test','test1','tr')
+   var cour =[
+    {
+        id : 1,
+        name:'C',
+        coin:10
+    },
+    {
+        id:6,
+        name:'D',
+        coin:15
+    },
+   ];
+function code(cour, index, originArray)
+{
+    return `<h2>${cour.name}</h2>` 
+    // {
+        // id:cour.id,
+        // name :`Khoa hoc:${course.name}`,
+        // coin:course.coin,
+        // coinText: `Gia : ${course.coin}`,
+        // index:index,
+        // originArray:originArray,
+
+   // }
+    console.log(c)
+}
+   var newCourses = cour.map(code);
+   console.log(newCourses)
+   console.log(newCourses.join(''));
+
+   // reduce
+   // Bien luu tru
+   var i = 0;
+   function coinHandler(accumulator, currentValue,currentIndex,originArray)
+   {
+        i++;
+        console.table({
+            'Luot chay:' :i,
+            'bien tich tru' : accumulator
+        })
+   }
+   var totalCoin = cour.reduce(
+
+   );
+   // Lap qua cac ptu
+
+
+   for(var course of cour){
+
+   }
